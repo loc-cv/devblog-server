e502bcf165621ada729211ee75beee283cdbfb36
loc-cv <donbb6824@gmail.com>
2022-11-12 12:51:12 +0700

    Fix report schema

--- src/schemas/report-schema.ts
+++ src/schemas/report-schema.ts
@@ -1,20 +1,22 @@
 import { z } from 'zod';
 
 export const newReportInputSchema = z.object({
-  postId: z
-    .string({
-      invalid_type_error: 'Post ID must be a string',
-      required_error: 'Post ID is required',
-    })
-    .min(1, { message: 'Post ID is required' }),
+  body: z.object({
+    postId: z
+      .string({
+        invalid_type_error: 'Post ID must be a string',
+        required_error: 'Post ID is required',
+      })
+      .min(1, { message: 'Post ID is required' }),
 
-  reason: z
-    .string({
-      invalid_type_error: 'Report reason must be a string',
-      required_error: 'Report reason is required',
-    })
-    .trim()
-    .min(1, { message: 'Report reason is required' }),
+    reason: z
+      .string({
+        invalid_type_error: 'Report reason must be a string',
+        required_error: 'Report reason is required',
+      })
+      .trim()
+      .min(1, { message: 'Report reason is required' }),
+  }),
 });
 
 export type NewReportInput = z.infer<typeof newReportInputSchema>;
